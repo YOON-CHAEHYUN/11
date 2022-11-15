f0e0ec4046c4ba85f06a74f535d61f7d33b2737e
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-15 16:56:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y){
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-}
-
-
 int main(int argc, char *argv[]) {
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a:%i, b=%i\n",a,b);
+	int i, j;
+	int grade[5];
+	
+	j=0;
+	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]=",i);
+		sacnf("%d",&grade[i]);
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]=%i\n",i,*(grade+i));
+		j+=*(grade+i);
+	}
+	printf("average: %i",j/5);
+	
 	return 0;
 }
