86fde76ae220997a30c2139403eebd032265b3cd
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-15 17:00:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,23 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i, j;
-	int grade[5];
+	char*pc;
+	int*pi;
+	double*pd;
 	
-	j=0;
+	pc=(char*)10000;
+	pi=(int*)10000;
+	pd=(double*)10000;
 	
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i]=",i);
-		sacnf("%d",&grade[i]);
-	}
+	printf("증가전: pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
 	
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i]=%i\n",i,*(grade+i));
-		j+=*(grade+i);
-	}
-	printf("average: %i",j/5);
+	pc++;
+	pi++;
+	pd++;
 	
+	printf("증가후: pc=%d, pi=%d, pd=%d\n",pc,pi,pd);
 	return 0;
 }
