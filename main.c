dcc01117578abb59cd9aecaf8da4619f7e82ebea
YOON-CHAEHYUN <yunch0202@naver.com>
2022-11-15 17:07:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+char*proverb="All that gliters is not gold";
+void setpointer(char*q)
+{
+	q=proverb;
+}
+
 int main(int argc, char *argv[]) {
-	int i=100;
-	int*p=&i;
-	int**q=&p;
+	char*p="zzz";
+	setpointer(p);
+	printf("%s\n",p);
 	
-	*p=200;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	 
-	**q=300;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	 
 	return 0;
 }
